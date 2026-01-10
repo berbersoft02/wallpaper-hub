ac6afcfdaeca33b93ccad5d2a05e8a4f695d9a95
Only_dias <markskills30@gmail.com>
2026-01-10 13:47:26 +0100

    Add Google AdSense script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { VT323, Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const pixelFont = VT323({
@@ -25,6 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7463641924793744"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={`${pixelFont.variable} ${bodyFont.variable} antialiased bg-gray-950 text-white font-body`}
       >
